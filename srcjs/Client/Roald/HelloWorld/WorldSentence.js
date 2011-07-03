52df4872dd7500991e3ac759d2530df96d7fcf8f
lars.roald <lars@timpex.no>
2011-07-03 13:07:23 +0200

    refactoed inheritane

--- srcjs/Client/Roald/HelloWorld/WorldSentence.js
+++ srcjs/Client/Roald/HelloWorld/WorldSentence.js
@@ -2,19 +2,37 @@ namespace("Roald.HelloWorld");
 
 Roald.HelloWorld.WorldSentence = function (context)
 {
+	
 	var instance = context || {};
-	var _hello = Roald.HelloWorld.NewHelloSentence(instance);
-	var _helloPrint = _hello.Print;
+	var _hello = undefined;
+	var _helloPrint = undefined;
+	var _goodbye = undefined;
+	var _goodbyePrint = undefined;
 
-	var _goodbye = Roald.HelloWorld.NewGoodbyeSentence(instance);
-	var _goodbyePrint = _goodbye.Print;
+	resolveHelloSentence();
+	resolveGoodbyeSentence();
+	setupInstance();
+	return instance;
+
+	function resolveHelloSentence() {
+		_hello = Roald.HelloWorld.NewHelloSentence(instance);
+		_helloPrint = _hello.Print;
+	}
+
+	function resolveGoodbyeSentence() {
+		_goodbye = Roald.HelloWorld.NewGoodbyeSentence(instance);
+		_goodbyePrint = _goodbye.Print;
+	}
+
+	function setupInstance() {
+		instance.Print = print;
+ 	}
+
 	
 	function print() {
 		return "World says " + _helloPrint() + " and " + _goodbyePrint();
 	}
 	
-	instance.Print = print;
- 	return instance;
 }
 
 Roald.HelloWorld.NewWorldSentence = function  (context)
